cabef95c64a212dbd9e0e5cca653f6ef91757d79
abhilash1200 <abhilashch@testvagrant.com>
2022-02-07 16:10:07 +0530

    Overriding getGstPercent Method to implement abstraction.

--- src/test/java/gstPercent/GstOnApparel.java
+++ src/test/java/gstPercent/GstOnApparel.java
@@ -2,6 +2,7 @@ package gstPercent;
 
 public class GstOnApparel extends BaseGstPercent {
 
+@Override
     public final int getGstPercent(){
         return 12;
     }
--- src/test/java/gstPercent/GstOnConsumerGoods.java
+++ src/test/java/gstPercent/GstOnConsumerGoods.java
@@ -2,6 +2,7 @@ package gstPercent;
 
 public class GstOnConsumerGoods extends BaseGstPercent {
 
+    @Override
     public final int getGstPercent(){
         return 5;
     }
